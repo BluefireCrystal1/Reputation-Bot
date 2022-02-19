38984378a7a1ec380646bd2aa010b39fd6abdaf4
BluefireCrystal1 <bluefirecrystal@outlook.com>
2022-02-19 11:06:57 +0530

    init

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,30 @@
+const { MessageEmbed } = require("discord.js")
+
 module.exports = {
     name: 'ping',
     description: 'Pong!',
     async execute(message, args, client) {
-        message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+        pingGreen = `Latency is ${Date.now() - message.createdTimestamp}ms. 
+                API Latency is ${Math.round(client.ws.ping)}ms`
+        pingRed = `Latency is ${Date.now() - message.createdTimestamp}ms. 
+                API Latency is ${Math.round(client.ws.ping)}ms`
+        
+
+        const embedGreen = new MessageEmbed()
+        .setTitle('Pong! 🏓')
+        .setDescription(pingGreen)
+        .setColor('#A2FF33')
+
+        const embedRed = new MessageEmbed()
+        .setTitle('Pong! 🏓')
+        .setDescription(pingRed)
+        .setColor('#FF4933')
+
+        if(Date.now() - message.createdTimestamp > 50) {
+            message.channel.send({embeds: [embedRed]})
+        }
+        if(Date.now() - message.createdTimestamp < 50) {
+            message.channel.send({embeds: [embedGreen]})
+        }
     }
 }
