b8df2f814d28cb1b3496f87fd0df1c21a928ecba
BluefireCrystal1 <bluefirecrystal@outlook.com>
2022-03-12 15:11:32 +0530

    init

--- commands/ping.js
+++ commands/ping.js
@@ -4,27 +4,31 @@ module.exports = {
     name: 'ping',
     description: 'Pong!',
     async execute(message, args, client) {
-        pingGreen = `Latency is ${Date.now() - message.createdTimestamp}ms. 
-                API Latency is ${Math.round(client.ws.ping)}ms`
-        pingRed = `Latency is ${Date.now() - message.createdTimestamp}ms. 
-                API Latency is ${Math.round(client.ws.ping)}ms`
-        
+        message.reply("Pinging...").then(msg => {
+            msg.edit("Pinging..")
+            msg.edit("Pinging.")
+            pingGreen = `\`Latency\` | ${Date.now() - message.createdTimestamp}ms. 
+                \`API Latency\` 🟢 | ${Math.round(client.ws.ping)}ms`
+            pingRed = `\`Latency\` | ${Date.now() - message.createdTimestamp}ms. 
+                \`API Latency\` 🔴 | ${Math.round(client.ws.ping)}ms`
 
-        const embedGreen = new MessageEmbed()
-        .setTitle('Pong! 🏓')
-        .setDescription(pingGreen)
-        .setColor('#A2FF33')
 
-        const embedRed = new MessageEmbed()
-        .setTitle('Pong! 🏓')
-        .setDescription(pingRed)
-        .setColor('#FF4933')
+            const embedGreen = new MessageEmbed()
+                .setTitle('Pong! 🏓')
+                .setDescription(pingGreen)
+                .setColor('#A2FF33')
 
-        if(Date.now() - message.createdTimestamp > 50) {
-            message.channel.send({embeds: [embedRed]})
-        }
-        if(Date.now() - message.createdTimestamp < 50) {
-            message.channel.send({embeds: [embedGreen]})
-        }
+            const embedRed = new MessageEmbed()
+                .setTitle('Pong! 🏓')
+                .setDescription(pingRed)
+                .setColor('#FF4933')
+
+            if (Date.now() - message.createdTimestamp > 50) {
+                msg.edit({ embeds: [embedRed], content: `<@!${message.author.id}>` })
+            }
+            if (Date.now() - message.createdTimestamp < 50) {
+                msg.edit({ embeds: [embedGreen], content: `<@!${message.author.id}>` })
+            }
+        })
     }
 }
