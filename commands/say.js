30f29f70966c6251794005b83215010250d77085
BluefireCrystal1 <bluefirecrystal@outlook.com>
2022-03-12 14:53:30 +0530

    init

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,17 @@
+const { MessageEmbed } = require("discord.js")
+
 module.exports = {
     name: 'say',
     description: 'Says something for you',
     async execute(message, args, client) {
-        message.reply(message.content)
+        const content = message.content.replace("+say", "")
+        const embed = new MessageEmbed()
+        .setAuthor({ name: message.member.user.username, iconURL: message.author.displayAvatarURL() })
+        .setDescription(content)
+        .setColor("#00C7FF")
+        .setTimestamp(message.createdTimestamp)
+        .setFooter({ text: `Message ID (Deleted): ${message.id}`, iconURL: message.guild.iconURL() })
+        message.channel.send({ embeds: [embed] })
+        message.delete()
     }
 }
